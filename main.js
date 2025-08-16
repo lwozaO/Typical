b57f18dfcc4279328508070976086e3724c8fd4d
lwozaO <113070096+lwozaO@users.noreply.github.com>
2025-08-16 21:22:45 +0900

    Test2

---
+++ main.js
@@ -0,0 +1,4 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+import App from "./App.js";
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
